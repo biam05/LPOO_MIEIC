53a2a971727a00cf096c75ec515646beaeceb2be
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2020-06-15 12:42:21 +0100

    started test logic circuits

---
+++ testes_praticos/2015/quiz/src/circuits/LogicVariable.java
@@ -0,0 +1,27 @@
+package circuits;
+
+public class LogicVariable {
+    private String name;
+    private boolean value;
+
+    public LogicVariable(String name, boolean value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean getValue() {
+        return value;
+    }
+
+    public void setValue(boolean value) {
+        this.value = value;
+    }
+}
